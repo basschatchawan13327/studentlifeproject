cf7559c647036a68a9d068d0eca7a01cab48819c
Chatchawan <basschatchawan13326@gmail.com>
2025-12-29 13:10:18 +0700

    merge expense, habit, week summary (Jenny Part)

--- StudentLifeTracker.py
+++ StudentLifeTracker.py
@@ -139,5 +139,82 @@ while True :                     #main menu loop
 #=================================Khanh part=============================================
 
 #=================================Jenny part=============================================
+    elif main_choice == "3" :                                    
+        print("1. Add\n2. List\n3. Total\n")
+        e_choice = get_input("Enter your choice: ")
 
+        if e_choice == "1" :
+            date = get_input("Date (YYYY-MM-DD): ")
+            category = get_input("Amount (VND): ")    
+            amount = get_input("Category: ")
+            expenses.append ({"date": date, "amount":amount, "category": category})
+
+        elif e_choice == "2" :
+            for e in expenses:
+                print("\nDate:", e["date"])
+                print("Category:", e["category"])
+                print("Amount: ", e["amount"])                     
+
+        elif e_choice == "3" :
+            total = 0
+            for e in expenses:
+                total = total + e["amount"]
+                print("Total Expenses:  " , format_vnd(total), "VND")
+
+    elif main_choice == "4" :
+        print("1. Add Habit\n2. Record Today\n3. View Stats\n")
+        h_choice = get_input("")
+
+        if h_choice == "1" :
+            habit_name = input("habit_name: ")
+            habits.append ({"name": habit_name, "count": 0})
+
+        elif h_choice == "2" :
+            index = int(get_input("Index to record: "))
+            if  0 <= index < len(habits):
+                habits[index]["count"] += 1
+            else:
+                print("Invalid index")
+
+        elif h_choice == "3" :
+            for h in habits:
+                print("\nName:", h["name"])
+                print("Count:", h["count"])             
+
+    elif main_choice == "5" :
+        start_date = datetime.strptime(get_input("Enter start date(YYYY-MM-DD) "), "%Y%m%d")
+        end_date = start_date + timedelta(days=6)
+            
+        print("Study Summary for week\n")
+        for s in student_sessions:
+            session_date = datetime.strptime(s['date'], "%Y%m%d")
+            if start_date <= session_date <= end_date:
+                print("\nDate:", s["date"])
+                print("Subject:", s["subject"])
+                print("Duration:", s["duration"])
+
+        print("Assignment Due this week")
+        for a in assignments:
+            assignment_date = datetime.strptime(a["due_date"], "%Y%m%d")
+            if start_date <= assignment_date <= end_date:
+                    print("Title:", a["title"])
+                    print("Due date:", a["due_date"])
+                    print("Done:", a["done"])
+
+        print("Expenses Summary for week")
+        total_week = 0
+        for e in expenses:
+            expense_date = datetime.strptime(e["date"], "%Y%m%d")
+            if start_date <= expense_date <= end_date:
+                print("\nDate:", e["date"])
+                print("Category:", e["category"])
+                print("Amount: ", e["amount"])
+
+            print("Habits Stats\n")
+            for h in habits:
+                print("Habit:", h["name"])
+                print("Times:", h["count"])
+
+#=================================End of Jenny part======================================
+    save_data(student_sessions, assignments, expenses, habits) #saving data after each loop iteration
 
